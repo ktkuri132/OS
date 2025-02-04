c616ade1273cf99dadd228751cbd90d136c43683
m <3457532883@qq.com>
2025-02-04 14:16:28 +0800

    更新

--- User/main.c
+++ User/main.c
@@ -1,8 +1,16 @@
+
 #include "stm32f4xx.h"
 #include "boot.h"
+#include "sys.h"
+#include "usart/usart.h"
+#include <stdio.h>
 int main()
 {
-    test();
+    bsp_usart_1_inti(115200);
+    while (1) {
+        printf("Hello World\n");
+    }
+
     return 0;
 }
 
